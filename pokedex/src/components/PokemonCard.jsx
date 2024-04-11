5b0369e8f8e930877465cc0bacb8cf3202cdb3a4
Anurag Lakra <anurag1013lakra@gmail.com>
2024-04-11 19:12:02 +0530

    updated pokemon card

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,10 +1,41 @@
-const PokemonCard = () => {
-    // Your component code here
+import React, { useState } from "react";
 
-    return (<>
-        
-        </>
-    )
+const PokemonCard = ({ data }) => {
+  // Your component code here
+
+  const [loading, setLoading] = useState(true);
+
+  console.log(data);
+  const name = data.species.name;
+  const height = data.height;
+  const weight = data.weight;
+  const spriteUrl = data.sprites.front_default;
+  const stats = {
+    hp: data.stats[0].base_stat,
+    attack: data.stats[1].base_stat,
+    defense: data.stats[2].base_stat,
+    speed: data.stats[5].base_stat,
+  };
+
+  return (
+    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
+      <div className="flex justify-between">
+        <div className="mb-4">
+          <h2 className="text-2xl mb-2">{name}</h2>
+          <p className="text-gray-700 text-base">Height: {height}</p>
+          <p className="text-gray-700 text-base">Weight: {weight}</p>
+        </div>
+        <img className="w-40 h-40" src={spriteUrl} alt={name} />
+      </div>
+      <div>
+        <h3 className="text-xl mb-2">Stats:</h3>
+        <p className="text-gray-700 text-base">HP: {stats.hp}</p>
+        <p className="text-gray-700 text-base">Attack: {stats.attack}</p>
+        <p className="text-gray-700 text-base">Defense: {stats.defense}</p>
+        <p className="text-gray-700 text-base">Speed: {stats.speed}</p>
+      </div>
+    </div>
+  );
 };
 
 export default PokemonCard;
--- pokedex/src/pages/Home.jsx
+++ pokedex/src/pages/Home.jsx
@@ -2,24 +2,63 @@ import React, { useState } from "react";
 import PokemonCard from "../components/PokemonCard";
 
 const Home = () => {
-    const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [isFound, setIsFound] = useState(false);
+  const [isNotFound, setIsNotFound] = useState(false);
 
-    // Your component logic here
+  const [pokemon, setPokemon] = useState(null);
+  // Your component logic here
+  const handleSearch = () => {
+    setIsFound(false);
+    setIsNotFound(false);
+    //use pookeapi to search for pokemon
+    fetch(`https://pokeapi.co/api/v2/pokemon/${searchText}`)
+      .then((response) => {
+        if (!response.ok) {
+          setPokemon(null);
+          setIsNotFound(true);
+          throw new Error("Pokemon not found");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPokemon(data);
+        // console.log(pokemon);
+        setIsFound(true);
+        console.log("found");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log("searching for ", searchText);
+  };
 
-    return (
-        <>
-            <div class='search w-full flex relative '>
-                <input class="searchTerm w-full "
-                    type="text"
-                    placeholder="Enter Pokemon name"
-                    value={searchText}
-                    onChange={(e) => setSearchText(e.target.value)}
-                />
-                <button type="submit"></button>
-            </div>
-            <PokemonCard />
-        </>
-    );
+  return (
+    <>
+      <div className="w-1/3 absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="search w-full flex relative">
+          <input
+            className="searchTerm w-full h-10 border-2 border-blue-400 border-r-0 p-1 rounded-l-md text-blue-600 outline-none focus:border-blue-400"
+            type="text"
+            placeholder="Enter Pokemon name"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            className="h-10 w-36 border border-blue-400 bg-blue-400 text-center text-white rounded-r-md cursor-pointer text-lg"
+            type="submit"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div className=" w-auto absolute top-1/3 left-1/2 transform -translate-x-1/2 ">
+        {isNotFound && <p className="text-red-500">Pokemon not found</p>}
+        {isFound && <PokemonCard data={pokemon} />}
+      </div>
+    </>
+  );
 };
 
 export default Home;
